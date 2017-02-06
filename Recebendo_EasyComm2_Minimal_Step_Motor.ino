e835c0dd1c9f2a9267396332b1fa2a4c56ba445c
wfp2002 <wfp2002@msn.com>
2017-02-06 17:21:25 -0200

    nokia 5110 display add

--- Recebendo_EasyComm2_Minimal_Step_Motor.ino
+++ Recebendo_EasyComm2_Minimal_Step_Motor.ino
@@ -1,4 +1,22 @@
 #include <AccelStepper.h>
+#include <LCD5110_Basic.h>
+
+
+LCD5110 tela(2,3,4,5,6);
+/*Cria objeto da classe LCD5110
+CLK – Pino 2
+DIN – Pino 3
+DC – Pino 4
+RST – Pino 5
+CE – Pino 6
+*/
+ 
+//Obtendo as fontes
+extern uint8_t SmallFont[];
+extern uint8_t MediumNumbers[];
+extern uint8_t BigNumbers[];// pin 2 - Serial clock out (SCLK)
+
+
 #define HALFSTEP 8
 
 #define motorPin5  8     // IN1 on the ULN2003 driver 2
@@ -9,15 +27,17 @@
 //Inicializa o motor nos pinos acima
 AccelStepper motor(HALFSTEP, motorPin5, motorPin7, motorPin6, motorPin8);
 
+
+
 //variaveis
 float graus360 = 4075.7728395;
-int velocidade = 1000; //velocidade do motor (passos por segundo)
+int velocidade = 500; //velocidade do motor (passos por segundo)
 int passos = 0; //Serve para manter o tracking do motor o quanto falta para terminar ate ir para o proximo local
 float passosConvertido = 0;
 float local;
 float anguloInicial = 0;
 float anguloFinal;
-
+int posicao;
 float angleAz, angleEl;
 
 #define Motor_Steps_Per_Revolution 200 //Steps Per Revolution of the motors. You may need to change this if using motors other than the ones supplied with the ThumbNet kit.
@@ -28,9 +48,36 @@ float angleAz, angleEl;
 
 void setup() {
   
-  delay(3000); 
-
-  motor.setMaxSpeed(2000.0);
+  delay(2000); 
+  tela.InitLCD(); //Inicializando o display
+  
+  
+  
+  
+  tela.setFont(SmallFont); //Definindo a fonte
+ //Escreve a palavra vida alinhada à esquerda começando na linha 0
+ 
+ tela.print("INICIANDO", LEFT, 0);
+ tela.print("O", CENTER, 20);
+ tela.print("LCD", RIGHT, 40);
+ delay(1000);
+ tela.clrScr();
+ 
+ //tela.print("Numero medio:", LEFT, 0);
+ //tela.setFont(MediumNumbers);
+ //tela.printNumI(123, CENTER, 16);
+ //delay(2000);
+ //tela.clrScr();
+ 
+ //tela.setFont(SmallFont);
+ //tela.print("Numero grande:", LEFT, 0);
+ //tela.setFont(BigNumbers);
+ //tela.printNumI(123, CENTER, 16);
+ //delay(2000);
+ //tela.clrScr();
+ 
+  
+  motor.setMaxSpeed(500.0);
   motor.move(-1);  //Isso foi necessario
   motor.setSpeed(velocidade);
   Serial.begin(19200);
@@ -47,15 +94,23 @@ if(angleAz == 0){
 //Nao faz nada  
 }
 
-else if (angleEl == 0){  
-  
-  //Colocar um IF aqui para fazer o mnotor para pq aqui depois q o satelite sai ele fica mandando pra posicao zero eternamente, colocar um AZ_MOTOR _ENAVBLE = LOW ou 
-  //se graus360-curret = 4075 nao faz nada.
-  motor.move(graus360 - motor.currentPosition());
-  motor.setSpeed(velocidade);
-  passos = motor.distanceToGo();
-  motor.runSpeedToPosition();
+else if (angleEl == 0){
 
+  posicao = (graus360 - motor.currentPosition());
+  //Serial.println(graus360);
+  //Serial.println(motor.currentPosition());
+  //Serial.println(posicao);
+  //Se posicao for igual 4075 quer dizer que o motor ja voltou ao ponto inicial depois da passagem do satelite e nao precisa ficar mandando a informacao de retorno eternamente ate ter a proxima elevacao
+  if (posicao == 0) {
+    //Nao faz nada
+      //Serial.println("Rotor na posicao 0");
+  } else {
+      //Serial.println("Voltando o rotor para posicao 0");
+      motor.move(graus360 - motor.currentPosition());
+      motor.setSpeed(velocidade);
+      passos = motor.distanceToGo();
+      motor.runSpeedToPosition();
+  }
   
 }
 
@@ -63,11 +118,30 @@ else {
   
   //primeira vez executada passos = 0
   if (passos == 0) {
-     Serial.print("Angulo Azimute: ");
-     Serial.println(angleAz);
-     Serial.print("Angulo Elevacao: ");
-     Serial.println(angleEl);
- 
+    
+     int val = analogRead(0);
+     
+     if (val > 0) {
+     angleAz = map(val, 0, 1023, 0, 360);
+     }
+       
+     tela.clrScr();
+     tela.setFont(SmallFont);
+     tela.print("Azimute  Graus", CENTER, 0);
+     tela.setFont(MediumNumbers);
+     tela.printNumF(angleAz,2, CENTER, 8);
+     tela.setFont(SmallFont);
+     tela.print("Elevacao Graus", CENTER,24);
+     tela.setFont(MediumNumbers);
+     tela.printNumF(angleEl,2, CENTER, 32);
+     
+     
+     //Serial.print("Angulo Azimute: ");
+     //Serial.println(angleAz);
+     //Serial.print("Angulo Elevacao: ");
+     //Serial.println(angleEl);
+     //Serial.print("Valor PIN 0: ");
+     //Serial.println(angleAz);
      local = 0;
      
      
@@ -81,7 +155,7 @@ else {
   //Serial.println(local - motor.currentPosition());
   
  
-  delay(3000);
+  delay(1000);
   
   grausParaPassos(angleAz);
   motor.move(local - motor.currentPosition());
@@ -148,4 +222,3 @@ void grausParaPassos(float par){
 local = ((par * graus360) / 360);  
     
 }
-
